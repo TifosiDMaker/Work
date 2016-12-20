b39274c8eab38ca84642fd4080c195f8d7e74e64
Tifosi Li <tifosi19921030@gmail.com>
2016-12-20 16:37:43 +0800

    V1.0 !

--- test8.py
+++ test8.py
@@ -1,1 +1,10 @@
-print("\\\\")
+from openpyxl import Workbook
+from openpyxl.styles import PatternFill
+
+wb = Workbook()
+ws = wb.active
+fil = PatternFill(start_color='FFFF00',end_color='FFFF00',fill_type='solid')
+#fill2 = PatternFill(start_color='000000',end_color='000000',fill_type='solid')
+
+ws['B1'].fill = fil
+wb.save('test2.xlsx')
