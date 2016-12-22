e067f3a7330d2a450c85c969daa9e9652cd3d544
Tifosi Li <tifosi19921030@gmail.com>
2016-12-22 11:55:23 +0800

    V1.1.2  optimize a bit

--- Copy_Column.py
+++ Copy_Column.py
@@ -3,43 +3,35 @@ from openpyxl import Workbook
 from openpyxl.styles import PatternFill
 from docx import Document
 import re
+import time
+
+start = time.time()
 
 #os part
 dest_dir = input('请输入外部审校文件所在路径.\n>')
-dest_dir = dest_dir.replace("\\","/")
-root = dest_dir
-for root, dirs, files in os.walk(dest_dir):
-    pass
-name = files[0]
-dest_path = os.path.join(root, name)
+dest_dir = dest_dir.replace("\\", "/")
 
 #excel part
 wb = Workbook()
 ws = wb.active
-fil = PatternFill(start_color='FFFF00',end_color='FFFF00',fill_type='solid')
-
-#word part
-doc = Document(dest_path)
-
+fil = PatternFill(start_color='FFFF00', end_color='FFFF00',fill_type='solid')
 
 #traverse files in dest_dir
-i = 1
 for root, dirs, files in os.walk(dest_dir):
     for name in files:
         dest_path = os.path.join(root, name)
         doc = Document(dest_path)
         t = doc.tables[0]
-        ws.append({'B':name})
-        k = 0
-        for haha in ws.rows:
-            k += 1
-        ws['B' + str(k)].fill = fil
+        ws.append({'B': name})
+        ws['B' + str(ws.max_row)].fill = fil
 
         for j in range(len(t.rows)):
             if t.cell(j, 2).text == "":
                 continue
             strr = str(t.cell(j, 2).text)
-            strr = re.sub('<'r'/?[a-z]{0,3}[0-9]{0,5}/?''>','',strr)
+            strr = re.sub('<'r'/?[a-z]{0,3}[0-9]{0,5}/?''>', '', strr)
             ws.append({'B': strr})
-            k += 1
 wb.save(dest_dir + '/删重文件.xlsx')
+end = time.time()
+cost = end - start
+print (cost)
